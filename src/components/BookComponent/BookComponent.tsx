f06c1255bf4e0fe5f26dd6c066c86429d6437a94
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2023-03-23 22:08:02 +0300

    add style to bookComponent

--- src/components/BookComponent/BookComponent.tsx
+++ src/components/BookComponent/BookComponent.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { FULL_DATA, INFO_BOOK } from "../../constants/api";
 import { getApiResource } from "../../utils/network";
-import styles from "./BookComponent.module.scss";
+
+import arrow from '../../assets/chevron-left.png'
 
 export const BookComponent: React.FC = () => {
   const [title, setTitle] = useState("");
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState([]);
   const [authors, setAuthors] = useState([]);
   const [description, setDescription] = useState("");
   const [image, setImage] = useState("");
@@ -26,15 +27,32 @@ export const BookComponent: React.FC = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex">
-      <div className="min-w-[25%]">
-        <img src={image} alt="" />
-      </div>
-      <div>
-        <p>{category}</p>
-        <h2>{title}</h2>
-        <p>{authors && authors.join(" / ")}</p>
-        <p dangerouslySetInnerHTML={{ __html: description }}></p>
+    <div className="container mx-auto pl-5 pr-5 mb-5">
+      <Link to={'/'} className='flex max-w-[150px] border border-solid border-black p-2 uppercase gap-2 items-center mt-5 mb-5 font-semibold'><img src={arrow} className='max-h-[20px]'/>to search</Link>
+      <div className="flex flex-col md:flex-row gap-5">
+        <div className="md:min-w-[18%] flex justify-center md:justify-start mt-10 md:mt-5">
+          <img
+            src={image}
+            alt=""
+            className="max-w-full object-contain object-left-top"
+          />
+        </div>
+        <div className="mt-5 flex flex-col gap-3">
+          <p className="text-gray-500 underline">{category && category.filter((el: string, index: number) => {
+          if (index < 3) return el;
+        })}</p>
+          <h2 className="font-bold font-sans text-lg">{title}</h2>
+          <p className="text-gray-800 font-medium">
+            {authors && authors.join(" / ")}
+          </p>
+          { description &&
+            <p
+            className="border border-solid border-black p-5"
+            dangerouslySetInnerHTML={{ __html: description }}
+          ></p>
+          }
+          
+        </div>
       </div>
     </div>
   );
